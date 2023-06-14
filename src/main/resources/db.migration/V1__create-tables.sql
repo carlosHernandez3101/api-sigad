4259d0c70a0d1800fca430265f08b5e4191b58b2
carlosHernandez3101 <cehernandez@unicauca.edu.co>
2023-06-14 15:16:18 -0500

    Add Migrations

---
+++ src/main/resources/db.migration/V1__create-tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE docentes (
+  id bigint NOT NULL auto_increment,
+  nombres varchar(100) NOT NULL,
+  apellidos varchar(100) NOT NULL,
+  tipoIdentificacion varchar(20) NOT NULL,
+  identificacion varchar(20) NOT NULL,
+  tipoDocente varchar(20) NOT NULL,
+  correoInstitucional varchar(100) NOT NULL,
+  ultimoTituloAcademico varchar(100) NOT NULL,
+  primary key (id)
+);
+
+CREATE TABLE usuarios (
+  id bigint NOT NULL auto_increment,
+  usuario varchar(100) NOT NULL,
+  contraseña varchar(100) NOT NULL,
+  rol varchar(100) NOT NULL,
+  docente_id bigint NOT NULL,
+  primary key (id),
+  foreign key (docente_id) references docentes(id)
+
+);
